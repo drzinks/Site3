ee8d972f62212772d878b89339b6d77c260af4ac
Golebiowski <michal.golebiowski@hpe.com>
2016-11-24 15:58:59 +0100

    more ui

--- src/main/java/com/drzinks/client/SiteUI.java
+++ src/main/java/com/drzinks/client/SiteUI.java
@@ -6,9 +6,14 @@ import com.drzinks.server.WelcomeController;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
 @SpringUI
 @Theme("valo")
@@ -27,23 +32,39 @@ public class SiteUI extends UI {
 	protected void init(VaadinRequest request) {
 		setupLayout();
 		addHeader();
+		addForm();
+		addContactList();
+		
 		
 	}
 
-	
 	private void setupLayout() {
 		layout = new VerticalLayout(); //alt shift l extr local var
+		layout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
 		setContent(layout);
-	
 	}
 	
 	private void addHeader() {
-//		Label label1 = new Label(welcomeController.returnSth());
-//		layout.addComponent(label1);		
-		Label label1 = new Label(welcomeController.getSthElse());
-		//Label label2 = new Label("restClient.getSthFromServer()");
+		Label label1 = new Label(welcomeController.getHeaderText());
+		label1.addStyleName(ValoTheme.LABEL_H1);
+		label1.setSizeUndefined();
 		layout.addComponent(label1);
-	//	layout.addComponent(label2);
+	}
+	
+	private void addForm(){
+		HorizontalLayout formLayout = new HorizontalLayout();
+		TextField textField = new TextField();
+		Button addButton = new Button("Add");
+		formLayout.setSpacing(true);
+		formLayout.setWidth("80%");
+		formLayout.addComponents(textField,addButton);
+		layout.addComponent(formLayout);
+		
+	}
+	
+	private void addContactList() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/main/java/com/drzinks/server/WelcomeController.java
+++ src/main/java/com/drzinks/server/WelcomeController.java
@@ -1,19 +1,11 @@
 package com.drzinks.server;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.springframework.stereotype.Component;
 
 @Component
-@Path("sth")
 public class WelcomeController {
 
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getSthElse(){
-		return "sth elese";
+	public String getHeaderText(){
+		return "Header text from server";
 	}
 }
